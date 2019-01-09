59d889aed12e56ef31244ee5a0f385405f56f61e
zzdaw <hohi@wp.pl>
2019-01-09 15:05:46 +0100

    add products

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
+const data = {
+    items: [
+        {
+            id: 1,
+            color: 'green',
+            name: 'cup',
+        },
+        {
+            id: 2,
+            color: 'red',
+            name: 'cup',
+        }
+    ]
+}
+const Items = ({ item }) => (
+    <div>
+        <h2>Product name: {item.name}</h2>
+        <p>Product color: {item.color}</p>
+    </div>
+)
+
 class ShoppingCart extends React.Component {
     state = {
-        availableProducts: 10,
-        shoppingCart: 0,
+        availableGreenCup: 10,
+        availableRedCup: 10,
+        shoppingCartGreen: 0,
+        shoppingCartRed: 0,
         isConfirmed: false
     }
-    handleSubtraction = () => {
+    handleSubtractionGreen = () => {
         this.setState({
-            shoppingCart: this.state.shoppingCart - 1
+            shoppingCartGreen: this.state.shoppingCartGreen - 1
         })
     }
-    handleAddition = () => {
+    handleAdditionGreen = () => {
         this.setState({
-            shoppingCart: this.state.shoppingCart + 1
+            shoppingCartGreen: this.state.shoppingCartGreen + 1
+        })
+    }
+    handleSubtractionRed = () => {
+        this.setState({
+            shoppingCartRed: this.state.shoppingCartRed - 1
+        })
+    }
+    handleAdditionRed = () => {
+        this.setState({
+            shoppingCartRed: this.state.shoppingCartRed + 1
         })
     }
     handleCheckboxConfirmation = () => {
@@ -23,19 +56,32 @@ class ShoppingCart extends React.Component {
         e.preventDefault()
         if (this.state.isConfirmed) {
             this.setState({
-                availableProducts: this.state.availableProducts - this.state.shoppingCart,
-                shoppingCart: 0,
+                availableGreenCup: this.state.availableGreenCup - this.state.shoppingCartGreen,
+                availableRedCup: this.state.availableRedCup - this.state.shoppingCartRed,
+                shoppingCartGreen: 0,
+                shoppingCartRed: 0,
                 isConfirmed: false
             })
+        } else {
+            alert('You must accept conditions.')
         }
     }
     render() {
-        const { availableProducts, shoppingCart, isConfirmed } = this.state
+        const { availableGreenCup, availableRedCup, shoppingCartGreen, shoppingCartRed, isConfirmed } = this.state
+        const items = this.props.data.items
+        const products = items.map(item => <Items item={item} />)
         return (
             <>
-                <button onClick={this.handleSubtraction} disabled={shoppingCart ? false : true}>-</button>
-                <span> {shoppingCart} </span>
-                <button onClick={this.handleAddition} disabled={shoppingCart === availableProducts ? true : false}>+</button>
+                {products}
+                <p>Green cup</p>
+                <button onClick={this.handleSubtractionGreen} disabled={shoppingCartGreen ? false : true}>-</button>
+                <span> {shoppingCartGreen} </span>
+                <button onClick={this.handleAdditionGreen} disabled={shoppingCartGreen === availableGreenCup ? true : false}>+</button>
+
+                <p>Red cup</p>
+                <button onClick={this.handleSubtractionRed} disabled={shoppingCartRed ? false : true}>-</button>
+                <span> {shoppingCartRed} </span>
+                <button onClick={this.handleAdditionRed} disabled={shoppingCartRed === availableRedCup ? true : false}>+</button>
                 <form onSubmit={this.sendForm}>
                     <input type="checkbox" id='conditions' checked={isConfirmed} onChange={this.handleCheckboxConfirmation} />
                     <label htmlFor="conditions">Accepts the conditions of the store.</label>
@@ -47,4 +93,4 @@ class ShoppingCart extends React.Component {
     }
 }
 
-ReactDOM.render(<ShoppingCart />, document.getElementById('root'))
+ReactDOM.render(<ShoppingCart data={data} />, document.getElementById('root'))
